27f87cd3a293a49a226ca17ab07a63f7ab874512
alun <alunfeixue@qq.com>
2017-07-18 16:46:02 +0800

    commit

---
+++ library/src/main/java/com/daohen/personal/toolbox/library/util/Files.java
@@ -0,0 +1,25 @@
+package com.daohen.personal.toolbox.library.util;
+
+/**
+ * Created by alun on 17/7/18.
+ */
+
+public class Files {
+
+    /**
+     * /data/data/<application package>/cache
+     * @return
+     */
+    public static String getCachePath(){
+        return Contexts.getContext().getCacheDir().getPath();
+    }
+
+    /**
+     * /data/data/<application package>/files
+     * @return
+     */
+    public static String getFilesPath(){
+        return Contexts.getContext().getFilesDir().getPath();
+    }
+
+}
