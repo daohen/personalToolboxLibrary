27e57e01df71449469ee14b64bd414770e1d0937
daohen <alunfeixue@qq.com>
2017-07-20 00:17:18 +0800

    commit

---
+++ library/src/main/java/com/daohen/personal/toolbox/library/util/SystemUtil.java
@@ -0,0 +1,51 @@
+package com.daohen.personal.toolbox.library.util;
+
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+public class SystemUtil {
+
+    /**
+     * 是否在主进程中
+     * @return
+     */
+    public static boolean inMainProcess() {
+        String packageName = Contexts.getContext().getPackageName();
+        String processName = getProcessName(Contexts.getContext());
+        return packageName.equals(processName);
+    }
+
+    /**
+     * 当前进程的名字
+     * @param context
+     * @return
+     */
+    public static final String getProcessName(Context context) {
+        String processName = null;
+
+        ActivityManager am = ((ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE));
+
+        while (true) {
+            for (ActivityManager.RunningAppProcessInfo info : am.getRunningAppProcesses()) {
+                if (info.pid == android.os.Process.myPid()) {
+                    processName = info.processName;
+                    break;
+                }
+            }
+
+            // go home
+            if (!Strings.isNull(processName)) {
+                return processName;
+            }
+
+            // take a rest and again
+            try {
+                Thread.sleep(100L);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
