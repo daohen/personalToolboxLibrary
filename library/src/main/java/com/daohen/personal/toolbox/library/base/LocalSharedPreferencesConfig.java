587f4f4a88d00af80d21cbbc72e21e69e77383fe
alun <alunfeixue@qq.com>
2017-09-05 11:06:54 +0800

    commit

---
+++ library/src/main/java/com/daohen/personal/toolbox/library/base/LocalSharedPreferencesConfig.java
@@ -0,0 +1,73 @@
+package com.daohen.personal.toolbox.library.base;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.daohen.personal.toolbox.library.util.Contexts;
+
+/**
+ * CREATE BY ALUN
+ * EMAIL: alunfeixue2011@gmail.com
+ * DATE : 2017/09/05 10:55
+ */
+
+public class LocalSharedPreferencesConfig {
+
+    private SharedPreferences sp;
+
+    public LocalSharedPreferencesConfig(String name){
+        sp = Contexts.get().getContext().getSharedPreferences(name, Context.MODE_PRIVATE);
+    }
+
+    public void saveString(String key, String value){
+        sp.edit().putString(key, value).apply();
+    }
+
+    public String getString(String key, String dValue){
+        return sp.getString(key, dValue);
+    }
+
+    public String getString(String key){
+        return getString(key, null);
+    }
+
+    public void saveBoolean(String key, boolean value){
+        sp.edit().putBoolean(key, value).apply();
+    }
+
+    public boolean getBoolean(String key, boolean dValue){
+        return sp.getBoolean(key, dValue);
+    }
+
+    public boolean getBoolean(String key){
+        return getBoolean(key, false);
+    }
+
+    public void saveInteger(String key, int value){
+        sp.edit().putInt(key, value).apply();
+    }
+
+    public int getInteger(String key, int dValue){
+        return sp.getInt(key, dValue);
+    }
+
+    public int getInteger(String key){
+        return getInteger(key, 0);
+    }
+
+    public void saveLong(String key, long value){
+        sp.edit().putLong(key, value).apply();
+    }
+
+    public long getLong(String key, long dvalue){
+        return sp.getLong(key, dvalue);
+    }
+
+    public long getLong(String key){
+        return getLong(key, 0L);
+    }
+
+    public SharedPreferences getSp(){
+        return sp;
+    }
+}
