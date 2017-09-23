a3f89017556ea30360e99bc5cd1ac8ecff3f20a2
alun <alunfeixue@qq.com>
2017-09-23 10:47:33 +0800

    commit

---
+++ library/src/main/java/com/daohen/personal/toolbox/library/function/Callback.java
@@ -0,0 +1,11 @@
+package com.daohen.personal.toolbox.library.function;
+
+/**
+ * CREATE BY ALUN
+ * EMAIL: alunfeixue2011@gmail.com
+ * DATE : 2017/09/23 10:47
+ */
+
+public interface Callback {
+    void run();
+}
